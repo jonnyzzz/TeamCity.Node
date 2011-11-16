b8c01e0ff53a371ed925c9406afc422b110268a0
Eugene.Petrenko <eugene.petrenko@gmail.com>
2011-11-16 17:17:49 +0100

    create test module, add necessary deps to use TeamCity tests API

---
+++ tests/src/SimpleTest.java
@@ -0,0 +1,13 @@
+import jetbrains.buildServer.BaseTestCase;
+import org.testng.annotations.Test;
+
+/**
+ * @author Eugene Petrenko (eugene.petrenko@gmail.com)
+ *         Date: 16.11.11 17:14
+ */
+public class SimpleTest extends BaseTestCase {
+  @Test
+  public void Test() {
+
+  }
+}
