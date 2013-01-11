058eb9d6303067568590a6bb8bcc3c7441d55740
Eugene Petrenko <eugene.petrenko@gmail.com>
2013-01-12 00:47:45 +0100

    add extension method for nullable strings (from StringUtil)

---
+++ common/src/com/jonnyzzz/teamcity/plugins/node/common/Extensions.kt
@@ -0,0 +1,9 @@
+package com.jonnyzzz.teamcity.plugins.node.common
+
+/**
+ * Created by Eugene Petrenko (eugene.petrenko@gmail.com)
+ * Date: 12.01.13 0:41
+ */
+
+public fun String?.isEmptyOrSpaces() : Boolean = com.intellij.openapi.util.text.StringUtil.isEmptyOrSpaces(this)
+
