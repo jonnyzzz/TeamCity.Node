4804acf669556a484939d4520a331c527a746b55
Eugene Petrenko <eugene.petrenko@gmail.com>
2013-08-17 01:13:36 +0200

    add buildprocess utils

---
+++ agent/src/com/jonnyzzz/teamcity/plugins/node/agent/processes/BuildProcess.kt
@@ -0,0 +1,94 @@
+package com.jonnyzzz.teamcity.plugins.node.agent.processes
+/*
+ * Copyright 2013-2013 Eugene Petrenko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import jetbrains.buildServer.RunBuildException
+import jetbrains.buildServer.agent.BuildFinishedStatus
+import jetbrains.buildServer.agent.BuildProcess
+import java.util.concurrent.atomic.AtomicBoolean
+import jetbrains.buildServer.RunBuildException
+
+
+public abstract class BuildProcessBase: BuildProcess {
+  private val myIsInterrupted: AtomicBoolean = AtomicBoolean()
+  private val myIsFinished: AtomicBoolean = AtomicBoolean()
+
+  public override fun isInterrupted(): Boolean {
+    return myIsInterrupted.get()
+  }
+
+  public override fun isFinished(): Boolean {
+    return myIsFinished.get()
+  }
+
+  public override fun interrupt() {
+    myIsInterrupted.set(true)
+    interruptImpl()
+  }
+
+  public override fun waitFor(): BuildFinishedStatus {
+    try
+    {
+      if (isInterrupted())
+        return BuildFinishedStatus.INTERRUPTED
+
+      val status = waitForImpl()
+      if (isInterrupted())
+        return BuildFinishedStatus.INTERRUPTED
+
+      return status
+    } finally {
+      myIsFinished.set(true)
+    }
+  }
+  protected abstract fun waitForImpl(): BuildFinishedStatus
+
+  protected open fun interruptImpl() { }
+  public override fun start() { }
+}
+
+public open class DelegatingBuildProcess(val action: DelegatingProcessAction): BuildProcessBase() {
+  private val myReference = AtomicReference<BuildProcess>()
+
+  protected override fun interruptImpl() {
+    super.interruptImpl()
+
+    val process = myReference.get()
+    if (process != null)
+      process.interrupt()
+  }
+
+  protected override fun waitForImpl(): BuildFinishedStatus {
+    try
+    {
+      val process = action.startImpl()
+      myReference.set(process)
+      if (isInterrupted())
+        return BuildFinishedStatus.INTERRUPTED
+
+      process.start()
+      return process.waitFor()
+    } finally {
+      myReference.set(null)
+      action.finishedImpl()
+    }
+  }
+}
+
+public trait DelegatingProcessAction {
+  open fun startImpl(): BuildProcess
+  open fun finishedImpl()
+}
---
+++ agent/src/com/jonnyzzz/teamcity/plugins/node/agent/processes/CompositeBuildProcess.kt
@@ -0,0 +1,77 @@
+package com.jonnyzzz.teamcity.plugins.node.agent.processes
+/*
+ * Copyright 2013-2013 Eugene Petrenko
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.util.concurrent.BlockingQueue
+import java.util.concurrent.LinkedBlockingQueue
+import java.util.concurrent.atomic.AtomicReference
+import jetbrains.buildServer.agent.BuildFinishedStatus
+import jetbrains.buildServer.agent.BuildProcess
+
+
+public trait BuildProcessContinuation {
+  fun pushBuildProcess(process:BuildProcess)
+}
+
+
+public trait CompositeBuildProcess : BuildProcess, BuildProcessContinuation {
+}
+
+public open class CompositeBuildProcessImpl : BuildProcessBase(), CompositeBuildProcess {
+  private val myProcessList: BlockingQueue<BuildProcess> = LinkedBlockingQueue<BuildProcess>()
+  private val myCurrentProcess: AtomicReference<BuildProcess> = AtomicReference<BuildProcess>()
+
+  public override fun pushBuildProcess(process: BuildProcess) {
+    myProcessList.add(process)
+  }
+
+  protected override fun interruptImpl() {
+    val process: BuildProcess? = myCurrentProcess.get()
+    if (process != null)
+      process.interrupt()
+  }
+
+  public override fun start() {
+  }
+
+  protected override fun waitForImpl(): BuildFinishedStatus {
+    if (isInterrupted())
+      return BuildFinishedStatus.INTERRUPTED
+
+    val proc = myProcessList.poll()
+    if (proc != null) {
+      myCurrentProcess.set(proc)
+      try {
+        proc.start()
+        val status = proc.waitFor()
+        if (status != BuildFinishedStatus.INTERRUPTED && status != BuildFinishedStatus.FINISHED_SUCCESS)
+          return status
+      } finally {
+        myCurrentProcess.set(null)
+      }
+
+      if (isInterrupted())
+        return BuildFinishedStatus.INTERRUPTED
+
+      return waitForImpl()
+    }
+
+    if (isInterrupted())
+      return BuildFinishedStatus.INTERRUPTED
+
+    return BuildFinishedStatus.FINISHED_SUCCESS
+  }
+}
