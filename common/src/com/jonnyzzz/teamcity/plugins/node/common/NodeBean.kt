bd48573a0a7cb36f8b4d8e31d77110c9f1fe024c
Eugene Petrenko <eugene.petrenko@gmail.com>
2013-08-21 10:58:30 +0200

    remove unused field

--- common/src/com/jonnyzzz/teamcity/plugins/node/common/NodeBean.kt
+++ common/src/com/jonnyzzz/teamcity/plugins/node/common/NodeBean.kt
@@ -28,7 +28,6 @@ public class NodeBean {
   public val executionModeScript: ExecutionModes = ExecutionModes.Script
   public val commandLineParameterKey: String = "node_execution_args"
   public val scriptParameterKey: String = "node_script_args"
-  public val runTypeName: String = "jonnyzzz.node"
   public val toolPathKey: String = "node_toolPath"
 
   public val phantomJsExtensionKey : String = "phantom_extension"
